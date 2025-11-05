a9647ce31c224478f5c3544fb0af9965d8c14138
Gabriel <greis826@email.com>
2025-11-05 18:56:21 -0300

    interface de contrato de instancia

---
+++ back-end/back-end/Model/Dao/INProdutoDao.cs
@@ -0,0 +1,14 @@
+﻿using back_end.Model.Entities;
+
+namespace back_end.Model.Dao
+{
+    public interface INProdutoDao
+    {
+        void Insert(ProdutoEntity produto);
+        void Update(ProdutoEntity produto);
+        void DeleById(int id);
+        ProdutoEntity FindById(int id);
+        List<ProdutoEntity> FindAll();
+        List<ProdutoEntity> FindBySelect(string select);
+    }
+}
--- back-end/back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d8382f3a3ababd9f0fd472919ae412ffedf6be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec2969da43e2dcfb7317d3c89b6d4a0cfeb6405")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
