1fd49f0a329ab76cc8916b6852be00e7a965acfb
Gabriel <greis826@email.com>
2025-11-06 18:08:58 -0300

    controle de exception na comnunicação com banco de dados

---
+++ back-end/back-end/Model/DataBase/DbException.cs
@@ -0,0 +1,11 @@
+﻿namespace back_end.Model.DataBase
+{
+    internal class DbException : Exception
+    {
+        public DbException() { }
+
+        public DbException(string message) : base(message) { }
+
+        public DbException(string message, Exception inner) : base(message, inner) { }
+    }
+}
--- back-end/back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9647ce31c224478f5c3544fb0af9965d8c14138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f6aff7bed4e7c83453de8b937a446b87d1bb01")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
