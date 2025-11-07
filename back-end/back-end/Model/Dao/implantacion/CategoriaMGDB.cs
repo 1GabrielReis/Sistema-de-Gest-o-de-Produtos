35e64d41eaba3fce4c0fc6949e5f8bdb44a3b0de
Gabriel <greis826@email.com>
2025-11-07 03:00:51 -0300

    metodo DindAll

--- back-end/back-end/Model/Dao/implantacion/CategoriaMGDB.cs
+++ back-end/back-end/Model/Dao/implantacion/CategoriaMGDB.cs
@@ -7,18 +7,23 @@ namespace back_end.Model.Dao.implantacion
 {
     public class CategoriaMGDB : INCategoriaDao
     {
-        private readonly IMongoDatabase? database;
+        private readonly IMongoDatabase database;
 
         public CategoriaMGDB(IMongoDatabase db)
         {
+            if (db == null)
+            {
+                throw new ArgumentNullException(nameof(db), "O banco de dados não pode ser nulo.");
+            }
             database = db;
         }
 
         public void Insert(CategoriaEntity categoria)
         {
+            var list= new List<CategoriaEntity>();
             try
             {
-
+                // Implementação do método Insert
             }
             catch (MongoException ex)
             {
@@ -45,8 +50,15 @@ namespace back_end.Model.Dao.implantacion
 
         public List<CategoriaEntity> FindAll()
         {
-            
-            return new List<CategoriaEntity>(); 
+            try
+            {
+                var categories = database.GetCollection<CategoriaEntity>("categories");
+                return categories.Find(_ => true).ToList();
+            }
+            catch (MongoException ex)
+            {
+                throw new CustomDbException(ex.Message);
+            } 
         }
 
         public List<CategoriaEntity> FindByname(string name)
@@ -54,6 +66,7 @@ namespace back_end.Model.Dao.implantacion
             
             return new List<CategoriaEntity>(); 
         }
+
     }
 }
 
